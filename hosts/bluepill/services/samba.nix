877422441575b42a93b180d3385a908d8cc29456
intergrav <devin@devins.page>
2025-12-12 00:29:07 -0500

    feat: finder rackmac icon
    hehe

--- hosts/bluepill/services/samba.nix
+++ hosts/bluepill/services/samba.nix
@@ -4,10 +4,26 @@
     nssmdns4 = true;
     publish = {
       enable = true;
+      workstation = true;
       userServices = true;
       hinfo = true;
     };
   };
+
+  services.avahi.extraServiceFiles = [
+    (pkgs.writeText "device-info.service" ''
+      <?xml version="1.0" standalone='no'?>
+      <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+      <service-group>
+        <name replace-wildcards="yes">%h</name>
+        <service>
+          <type>_device-info._tcp</type>
+          <port>0</port>
+          <txt-record>model=RackMac</txt-record>
+        </service>
+      </service-group>
+    '')
+  ];
 
   services.samba-wsdd = {
     enable = true;
@@ -22,7 +38,7 @@
         # https://wiki.samba.org/index.php/Configure_Samba_to_Work_Better_with_Mac_OS_X
         "vfs objects" = "catia fruit streams_xattr"; # added catia
         "fruit:metadata" = "stream";
-        "fruit:model" = "Macmini";
+        "fruit:model" = "RackMac";
         "fruit:veto_appledouble" = "no";
         "fruit:nfs_aces" = "no";
         "fruit:wipe_intentionally_left_blank_rfork" = "yes";
