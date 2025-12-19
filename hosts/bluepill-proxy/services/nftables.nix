3320497aaaa6dc878a899448ee8864d5d80b82c2
intergrav <devin@devins.page>
2025-12-18 23:44:00 -0500

    fix: bleh

--- hosts/bluepill-proxy/services/nftables.nix
+++ hosts/bluepill-proxy/services/nftables.nix
@@ -1,10 +1,10 @@
-{
-  networking = {
-    sysctl = {
-      "net.ipv4.ip_forward" = 1;
-      "net.ipv6.conf.all.forwarding" = 1;
-    };
+{pkgs, ...}: {
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv6.conf.all.forwarding" = 1;
+  };
 
+  networking = {
     firewall = {
       enable = true;
       allowedTCPPorts = [25565];
