66f6603b6b45563c5f9d342e7b8f34890c3de4e9
intergrav <devin@devins.page>
2025-12-19 13:03:14 -0500

    fix: disable networkmanager

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -2,7 +2,6 @@
   system.stateVersion = lib.mkDefault "24.11";
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
-  networking.networkmanager.enable = true;
   zramSwap.enable = true;
   time.timeZone = lib.mkDefault "America/Nassau";
   i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
