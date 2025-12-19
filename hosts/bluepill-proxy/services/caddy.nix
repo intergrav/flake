1f6e3b0d6f71176f4514569e4cbc7834249d98cc
intergrav <devin@devins.page>
2025-12-18 23:26:30 -0500

    chore: remove gzip
    not needed

--- hosts/bluepill-proxy/services/caddy.nix
+++ hosts/bluepill-proxy/services/caddy.nix
@@ -6,21 +6,18 @@
     virtualHosts = {
       "navidrome.devins.page" = {
         extraConfig = ''
-          encode gzip
           reverse_proxy http://100.108.47.83:4533
         '';
       };
 
       "jellyfin.devins.page" = {
         extraConfig = ''
-          encode gzip
           reverse_proxy http://100.108.47.83:8096
         '';
       };
 
       "slskd.devins.page" = {
         extraConfig = ''
-          encode gzip
           reverse_proxy http://100.108.47.83:5030
         '';
       };
