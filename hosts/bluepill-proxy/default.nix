828b78e2ec31bbad0e25400aebaebc0b6196fcfc
intergrav <devin@devins.page>
2025-12-18 23:42:20 -0500

    fix: stuff

--- hosts/bluepill-proxy/default.nix
+++ hosts/bluepill-proxy/default.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{...}: {
   imports = [
     ./hardware.nix
     ./services/caddy.nix
--- hosts/bluepill-proxy/services/nftables.nix
+++ hosts/bluepill-proxy/services/nftables.nix
@@ -1,32 +1,44 @@
-{pkgs, ...}: {
-  networking.sysctl = {
-    "net.ipv4.ip_forward" = 1;
-    "net.ipv6.conf.all.forwarding" = 1;
-  };
+{
+  networking = {
+    sysctl = {
+      "net.ipv4.ip_forward" = 1;
+      "net.ipv6.conf.all.forwarding" = 1;
+    };
 
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [25565];
-    allowedUDPPorts = [25565 24454 19132];
-  };
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [25565];
+      allowedUDPPorts = [25565 24454 19132];
 
-  networking.nftables = {
-    enable = true;
-    extraRules = ''
-      table ip nat {
-        chain prerouting {
-          type nat hook prerouting priority dstnat; policy accept;
-          tcp dport 25565 dnat to 100.109.134.42:25565;
-          udp dport 25565 dnat to 100.109.134.42:25565;
-          udp dport 24454 dnat to 100.109.134.42:24454;
-          udp dport 19132 dnat to 100.109.134.42:19132;
+      extraCommands = ''
+        nft add rule ip nat postrouting oif tailscale0 ip daddr 100.109.134.42 counter snat to 100.108.47.83
+      '';
+    };
+
+    nat = {
+      enable = true;
+      forwardPorts = [
+        {
+          sourcePort = 25565;
+          proto = "tcp";
+          destination = "100.109.134.42:25565";
         }
-
-        chain postrouting {
-          type nat hook postrouting priority srcnat; policy accept;
-          ip daddr 100.109.134.42 snat to 100.108.47.83;
+        {
+          sourcePort = 25565;
+          proto = "udp";
+          destination = "100.109.134.42:25565";
         }
-      }
-    '';
+        {
+          sourcePort = 24454;
+          proto = "udp";
+          destination = "100.109.134.42:24454";
+        }
+        {
+          sourcePort = 19132;
+          proto = "udp";
+          destination = "100.109.134.42:19132";
+        }
+      ];
+    };
   };
 }
