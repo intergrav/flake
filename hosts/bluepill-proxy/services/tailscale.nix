e55ce42cea15c85028fe28b912b1a1ac348b677b
intergrav <devin@devins.page>
2025-12-19 00:34:06 -0500

    fix: bleh

--- hosts/bluepill-proxy/services/tailscale.nix
+++ hosts/bluepill-proxy/services/tailscale.nix
@@ -1,15 +1,15 @@
-{...}: {
+{pkgs, ...}: {
   boot.kernel.sysctl = {
     "net.ipv4.ip_forward" = 1;
     "net.ipv6.conf.all.forwarding" = 1;
   };
 
   systemd.services.enable-gro = {
-    description = "enable gro on ens6";
+    description = "Enable GRO on ens6";
     after = ["network.target"];
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = "/sbin/ethtool -K ens6 gro on";
+      ExecStart = "${pkgs.ethtool}/bin/ethtool -K ens6 gro on";
     };
     wantedBy = ["multi-user.target"];
   };
