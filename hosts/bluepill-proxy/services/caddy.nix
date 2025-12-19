dbd61142a04aa415bf77fdc8f64c56a434923b27
intergrav <devin@devins.page>
2025-12-18 23:11:00 -0500

    fix: bleh

--- hosts/bluepill-proxy/services/caddy.nix
+++ hosts/bluepill-proxy/services/caddy.nix
@@ -7,21 +7,21 @@
       "navidrome.devins.page" = {
         extraConfig = ''
           encode gzip
-          reverse_proxy http://100.64.12.34:4533
+          reverse_proxy http://100.108.47.83:4533
         '';
       };
 
       "jellyfin.devins.page" = {
         extraConfig = ''
           encode gzip
-          reverse_proxy http://100.64.12.34:8096
+          reverse_proxy http://100.108.47.83:8096
         '';
       };
 
       "slskd.devins.page" = {
         extraConfig = ''
           encode gzip
-          reverse_proxy http://100.64.12.34:5030
+          reverse_proxy http://100.108.47.83:5030
         '';
       };
     };
