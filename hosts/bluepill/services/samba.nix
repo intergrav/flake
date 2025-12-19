9f32fd8a0a0fdd3376cc1d9d85429cd21ff33688
intergrav <devin@devins.page>
2025-12-19 16:42:27 -0500

    fix: test something

--- hosts/bluepill/services/samba.nix
+++ hosts/bluepill/services/samba.nix
@@ -69,7 +69,7 @@
 
   # NFS shares (testing)
 
-  networking.firewall.interfaces.tailscale0 = {
+  networking.firewall = {
     allowedTCPPorts = [111 2049 4000 4001 4002 20048];
     allowedUDPPorts = [111 2049 4000 4001 4002 20048];
   };
