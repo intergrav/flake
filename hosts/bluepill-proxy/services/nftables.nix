b292cb579a088fecd0d08a8d134fc8d45a5a71de
intergrav <devin@devins.page>
2025-12-18 23:49:23 -0500

    fix: add nftables

--- hosts/bluepill-proxy/services/nftables.nix
+++ hosts/bluepill-proxy/services/nftables.nix
@@ -1,8 +1,12 @@
-{...}: {
+{pkgs, ...}: {
   boot.kernel.sysctl = {
     "net.ipv4.ip_forward" = 1;
     "net.ipv6.conf.all.forwarding" = 1;
   };
+
+  environment.systemPackages = with pkgs; [
+    nftables
+  ];
 
   networking.firewall = {
     allowedTCPPorts = [25565];
