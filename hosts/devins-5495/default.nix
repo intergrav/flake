7d5f1fb0addd0faf563c6469df40af862845e39a
intergrav <devin@devins.page>
2025-12-29 23:42:34 -0500

    feat: ozone

--- hosts/devins-5495/default.nix
+++ hosts/devins-5495/default.nix
@@ -16,5 +16,27 @@
   networking.hostName = "devins-5495";
   networking.networkmanager.enable = true;
 
+  services.displayManager.gdm.enable = true;
+  programs.niri.enable = true;
+  security.polkit.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.swaylock = {};
+  programs.waybar.enable = true;
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  environment.systemPackages = with pkgs; [
+    firefox
+    (discord.override {
+      withOpenASAR = true;
+      withVencord = true;
+    })
+    prismlauncher
+    alacritty
+    fuzzel
+    swaylock
+    mako
+    swayidle
+  ];
+
   system.stateVersion = "25.11";
 }
