b3b7b8ed9ef8acb36b08a8acc931481c8b44ba02
intergrav <devin@devins.page>
2026-01-01 11:07:25 -0500

    chore: cleanup

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nixpkgs-tlp-pr.url = "github:NixOS/nixpkgs/pull/473626/head";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -21,69 +20,62 @@
   };
 
   outputs = {
-    self,
     nixpkgs,
     nixpkgs-stable,
-    nixpkgs-tlp-pr,
     nix-darwin,
     home-manager,
     home-manager-stable,
     mac-app-util,
     nixos-wsl,
     agenix,
-  }: let
-    commonModules = [./modules/common];
-  in {
+    ...
+  }: {
     darwinConfigurations.neo = nix-darwin.lib.darwinSystem {
-      modules =
-        commonModules
-        ++ [
-          ./modules/darwin
-          ./hosts/neo
-          home-manager.darwinModules.home-manager
-          mac-app-util.darwinModules.default
-          {home-manager.sharedModules = [mac-app-util.homeManagerModules.default];}
-        ];
+      modules = [
+        ./modules/common
+        ./modules/darwin
+        ./hosts/neo
+        home-manager.darwinModules.home-manager
+        mac-app-util.darwinModules.default
+        {home-manager.sharedModules = [mac-app-util.homeManagerModules.default];}
+      ];
     };
 
     nixosConfigurations = {
-      trinity = nixpkgs-tlp-pr.lib.nixosSystem {
-        modules =
-          commonModules
-          ++ [
-            ./modules/nixos
-            ./hosts/trinity
-            home-manager.nixosModules.home-manager
-          ];
+      trinity = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./modules/common
+          ./modules/nixos
+          ./hosts/trinity
+          home-manager.nixosModules.home-manager
+        ];
       };
 
       bluepill = nixpkgs-stable.lib.nixosSystem {
-        modules =
-          commonModules
-          ++ [
-            ./modules/nixos
-            ./hosts/bluepill
-            agenix.nixosModules.default
-            home-manager-stable.nixosModules.home-manager
-            {
-              nixpkgs.overlays = [
-                (final: prev: {
-                  tailscale = nixpkgs.legacyPackages.${prev.stdenv.hostPlatform.system}.tailscale;
-                })
-              ];
-            }
-          ];
+        modules = [
+          ./modules/common
+          ./modules/nixos
+          ./hosts/bluepill
+          agenix.nixosModules.default
+          home-manager-stable.nixosModules.home-manager
+          {
+            nixpkgs.overlays = [
+              (final: prev: {
+                tailscale = nixpkgs.legacyPackages.${prev.stdenv.hostPlatform.system}.tailscale;
+              })
+            ];
+          }
+        ];
       };
 
       tank-wsl = nixpkgs.lib.nixosSystem {
-        modules =
-          commonModules
-          ++ [
-            ./modules/nixos
-            ./hosts/tank-wsl
-            home-manager.nixosModules.home-manager
-            nixos-wsl.nixosModules.default
-          ];
+        modules = [
+          ./modules/common
+          ./modules/nixos
+          ./hosts/tank-wsl
+          home-manager.nixosModules.home-manager
+          nixos-wsl.nixosModules.default
+        ];
       };
     };
 
