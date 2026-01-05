9a102061be92da663470437e5d89c0b7e2ecaafd
intergrav <devin@devins.page>
2026-01-04 19:22:59 -0500

    chore: remove caddy

--- modules/nixos/services/ollama.nix
+++ modules/nixos/services/ollama.nix
@@ -1,6 +1,4 @@
 {pkgs, ...}: {
-  networking.firewall.allowedTCPPorts = [8443];
-
   services.ollama = {
     enable = true;
     loadModels = ["deepseek-r1:latest" "deepseek-v2:latest" "qwen3-coder:latest" "qwen3-vl:latest" "qwen3:latest" "gemma3:latest" "nemotron-3-nano:latest" "functiongemma:latest" "olmo-3:latest" "gtp-oss:latest" "llama3:latest" "devstral-small-2:latest" "mistral:latest" "mistral-nemo:latest"];
@@ -24,13 +22,5 @@
         autoStart = true;
       };
     };
-  };
-
-  services.caddy = {
-    enable = true;
-    virtualHosts.":8443".extraConfig = ''
-      encode gzip zstd
-      reverse_proxy 127.0.0.1:8080
-    '';
   };
 }
