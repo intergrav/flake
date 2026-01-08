6b4b34c81f4e6cb1d8ed1e8d943c015927b514c2
intergrav <devin@devins.page>
2026-01-08 18:11:02 -0500

    chore: disable ollama
    meh

--- hosts/bluepill/default.nix
+++ hosts/bluepill/default.nix
@@ -6,7 +6,6 @@
     ../../modules/nixos/services/mikusmp67.nix
     ../../modules/nixos/services/navidrome.nix
     ../../modules/nixos/services/newbeginnings7.nix
-    ../../modules/nixos/services/ollama.nix
     ../../modules/nixos/services/restic.nix
     ../../modules/nixos/services/samba.nix
     ../../modules/nixos/services/slskd.nix
