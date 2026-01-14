dadfef77295c18bb2876d28167a23f306c118e25
intergrav <devin@devins.page>
2026-01-13 22:01:14 -0500

    chore: remove nixpkgs-stable and home-manager-stable
    i dont use them anymore, i find unstable nixpkgs perfectly fine for my
    server and it's less maintenance in the future

--- flake.nix
+++ flake.nix
@@ -1,14 +1,9 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
-    };
-    home-manager-stable = {
-      url = "github:nix-community/home-manager/release-25.11";
-      inputs.nixpkgs.follows = "nixpkgs-stable";
     };
     nix-darwin = {
       url = "github:nix-darwin/nix-darwin";
@@ -20,10 +15,8 @@
 
   outputs = {
     nixpkgs,
-    nixpkgs-stable,
     nix-darwin,
     home-manager,
-    home-manager-stable,
     mac-app-util,
     agenix,
     ...
