761b15266d301e5356de41872a7d4177d9a8352a
intergrav <devin@devins.page>
2026-01-20 20:07:17 -0500

    feat: fix tailscale things

--- modules/nixos/services/tailscale.nix
+++ modules/nixos/services/tailscale.nix
@@ -1,3 +1,17 @@
-{...}: {
-  services.tailscale.enable = true;
+{pkgs, ...}: {
+  services.tailscale = {
+    enable = true;
+    useRoutingFeatures = "both";
+    openFirewall = true;
+  };
+
+  services.networkd-dispatcher = {
+    enable = true;
+    rules."50-tailscale-optimizations" = {
+      onState = ["routable"];
+      script = ''
+        ${pkgs.ethtool}/bin/ethtool -K eth0 rx-udp-gro-forwarding on rx-gro-list off
+      '';
+    };
+  };
 }
