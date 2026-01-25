21b548fd2d9b00101953706cb7754f9dc907c66d
intergrav <devin@devins.page>
2026-01-25 04:02:16 -0500

    fix: sh

--- hosts/bluepill-proxy/modules/socat.nix
+++ hosts/bluepill-proxy/modules/socat.nix
@@ -12,16 +12,14 @@
     after = ["network-online.target"];
     serviceConfig = {
       ExecStart = ''
-        sh -c '
-          socat TCP-LISTEN:25565,reuseaddr,fork TCP:100.108.47.83:25565 &
-          socat UDP-LISTEN:25565,reuseaddr,fork UDP:100.108.47.83:25565 &
-          socat UDP-LISTEN:24454,reuseaddr,fork UDP:100.108.47.83:24454 &
+        socat TCP-LISTEN:25565,reuseaddr,fork TCP:100.108.47.83:25565 &
+        socat UDP-LISTEN:25565,reuseaddr,fork UDP:100.108.47.83:25565 &
+        socat UDP-LISTEN:24454,reuseaddr,fork UDP:100.108.47.83:24454 &
 
-          socat TCP-LISTEN:23343,reuseaddr,fork TCP:100.108.47.83:23343 &
-          socat UDP-LISTEN:23343,reuseaddr,fork UDP:100.108.47.83:23343 &
-          socat UDP-LISTEN:22232,reuseaddr,fork UDP:100.108.47.83:22232 &
-          wait
-        '
+        socat TCP-LISTEN:23343,reuseaddr,fork TCP:100.108.47.83:23343 &
+        socat UDP-LISTEN:23343,reuseaddr,fork UDP:100.108.47.83:23343 &
+        socat UDP-LISTEN:22232,reuseaddr,fork UDP:100.108.47.83:22232 &
+        wait
       '';
       Restart = "always";
       User = "nobody";
