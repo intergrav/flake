3be112f0c1e77c391e20c40198b012f00fa33c42
intergrav <devin@devins.page>
2026-01-25 15:58:02 -0500

    fix: idk bro

--- hosts/bluepill/modules/pds.nix
+++ hosts/bluepill/modules/pds.nix
@@ -11,7 +11,7 @@
     settings = {
       PDS_PORT = 3000;
       PDS_HOSTNAME = "pds.devins.page";
-      PDS_SERVICE_HANDLE_DOMAINS = "devins.page";
+      PDS_SERVICE_HANDLE_DOMAINS = ".devins.page";
       PDS_ADMIN_EMAIL = "devin@devins.page";
     };
   };
