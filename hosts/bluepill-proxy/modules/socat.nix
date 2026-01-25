b2fb20f6922c479a7b08b0457834aa338ec8fc50
intergrav <devin@devins.page>
2026-01-25 04:12:07 -0500

    fix: whatever

--- hosts/bluepill-proxy/modules/socat.nix
+++ hosts/bluepill-proxy/modules/socat.nix
@@ -5,24 +5,84 @@
     allowedUDPPorts = [25565 23343 24454 22232];
   };
 
-  systemd.services.minecraft-forward = {
-    description = "Minecraft TCP+UDP forward";
+  # TCP ports
+  systemd.services.minecraft-tcp-25565 = {
+    description = "Minecraft TCP 25565 forward";
     wants = ["network-online.target"];
     after = ["network-online.target"];
     serviceConfig = {
-      ExecStart = ''
-        ${pkgs.bash}/bin/bash -c "
-          ${pkgs.socat}/bin/socat TCP-LISTEN:25565,reuseaddr,fork TCP:100.108.47.83:25565 &
-          ${pkgs.socat}/bin/socat UDP-LISTEN:25565,reuseaddr,fork UDP:100.108.47.83:25565 &
-          ${pkgs.socat}/bin/socat UDP-LISTEN:24454,reuseaddr,fork UDP:100.108.47.83:24454 &
+      ExecStart = "${pkgs.socat}/bin/socat TCP-LISTEN:25565,reuseaddr,fork TCP:100.108.47.83:25565";
+      Restart = "always";
+      User = "nobody";
+      StandardOutput = "journal";
+      StandardError = "journal";
+    };
+    wantedBy = ["multi-user.target"];
+  };
 
-          ${pkgs.socat}/bin/socat TCP-LISTEN:23343,reuseaddr,fork TCP:100.108.47.83:23343 &
-          ${pkgs.socat}/bin/socat UDP-LISTEN:23343,reuseaddr,fork UDP:100.108.47.83:23343 &
-          ${pkgs.socat}/bin/socat UDP-LISTEN:22232,reuseaddr,fork UDP:100.108.47.83:22232 &
+  systemd.services.minecraft-tcp-23343 = {
+    description = "Minecraft TCP 23343 forward";
+    wants = ["network-online.target"];
+    after = ["network-online.target"];
+    serviceConfig = {
+      ExecStart = "${pkgs.socat}/bin/socat TCP-LISTEN:23343,reuseaddr,fork TCP:100.108.47.83:23343";
+      Restart = "always";
+      User = "nobody";
+      StandardOutput = "journal";
+      StandardError = "journal";
+    };
+    wantedBy = ["multi-user.target"];
+  };
 
-          wait
-        "
-      '';
+  # UDP ports
+  systemd.services.minecraft-udp-25565 = {
+    description = "Minecraft UDP 25565 forward";
+    wants = ["network-online.target"];
+    after = ["network-online.target"];
+    serviceConfig = {
+      ExecStart = "${pkgs.socat}/bin/socat UDP-LISTEN:25565,reuseaddr,fork UDP:100.108.47.83:25565";
+      Restart = "always";
+      User = "nobody";
+      StandardOutput = "journal";
+      StandardError = "journal";
+    };
+    wantedBy = ["multi-user.target"];
+  };
+
+  systemd.services.minecraft-udp-24454 = {
+    description = "Minecraft UDP 24454 forward";
+    wants = ["network-online.target"];
+    after = ["network-online.target"];
+    serviceConfig = {
+      ExecStart = "${pkgs.socat}/bin/socat UDP-LISTEN:24454,reuseaddr,fork UDP:100.108.47.83:24454";
+      Restart = "always";
+      User = "nobody";
+      StandardOutput = "journal";
+      StandardError = "journal";
+    };
+    wantedBy = ["multi-user.target"];
+  };
+
+  systemd.services.minecraft-udp-23343 = {
+    description = "Minecraft UDP 23343 forward";
+    wants = ["network-online.target"];
+    after = ["network-online.target"];
+    serviceConfig = {
+      ExecStart = "${pkgs.socat}/bin/socat UDP-LISTEN:23343,reuseaddr,fork UDP:100.108.47.83:23343";
+      Restart = "always";
+      User = "nobody";
+      StandardOutput = "journal";
+      StandardError = "journal";
+    };
+    wantedBy = ["multi-user.target"];
+  };
+
+  systemd.services.minecraft-udp-22232 = {
+    description = "Minecraft UDP 22232 forward";
+    wants = ["network-online.target"];
+    after = ["network-online.target"];
+    serviceConfig = {
+      ExecStart = "${pkgs.socat}/bin/socat UDP-LISTEN:22232,reuseaddr,fork UDP:100.108.47.83:22232";
       Restart = "always";
       User = "nobody";
       StandardOutput = "journal";
