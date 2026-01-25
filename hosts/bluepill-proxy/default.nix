0851a0adf3ecdb235ad932913e8f521f104a3bba
intergrav <devin@devins.page>
2026-01-25 00:14:09 -0500

    chore: update bluepill-proxy

--- hosts/bluepill-proxy/default.nix
+++ hosts/bluepill-proxy/default.nix
@@ -4,7 +4,7 @@
   ];
 
   networking.hostName = "bluepill-proxy";
-  networking.domain = "vps-6ad86777.vps.ovh.us";
+  networking.domain = "vps.ovh.us";
 
   users.users.root.openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILyOJLlmq653uVs/1KdVnaMzi+EolB7EJMJ7AdBxVi6m devin@devins.page''];
   users.users.devin.openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILyOJLlmq653uVs/1KdVnaMzi+EolB7EJMJ7AdBxVi6m devin@devins.page''];
