f59e87c0f7d6da44aa04f635e75a612a0d3e6e4e
intergrav <devin@devins.page>
2026-01-25 04:07:39 -0500

    fix: ughhh pmo

--- hosts/bluepill-proxy/modules/socat.nix
+++ hosts/bluepill-proxy/modules/socat.nix
@@ -10,14 +10,16 @@
     after = ["network-online.target"];
     serviceConfig = {
       ExecStart = ''
-        ${pkgs.socat}/bin/socat TCP-LISTEN:25565,reuseaddr,fork TCP:100.108.47.83:25565 &
-        ${pkgs.socat}/bin/socat UDP-LISTEN:25565,reuseaddr,fork UDP:100.108.47.83:25565 &
-        ${pkgs.socat}/bin/socat UDP-LISTEN:24454,reuseaddr,fork UDP:100.108.47.83:24454 &
+        sh -c "
+          ${pkgs.socat}/bin/socat TCP-LISTEN:25565,reuseaddr,fork TCP:100.108.47.83:25565 &
+          ${pkgs.socat}/bin/socat UDP-LISTEN:25565,reuseaddr,fork UDP:100.108.47.83:25565 &
+          ${pkgs.socat}/bin/socat UDP-LISTEN:24454,reuseaddr,fork UDP:100.108.47.83:24454 &
 
-        ${pkgs.socat}/bin/socat TCP-LISTEN:23343,reuseaddr,fork TCP:100.108.47.83:23343 &
-        ${pkgs.socat}/bin/socat UDP-LISTEN:23343,reuseaddr,fork UDP:100.108.47.83:23343 &
-        ${pkgs.socat}/bin/socat UDP-LISTEN:22232,reuseaddr,fork UDP:100.108.47.83:22232 &
-        wait
+          ${pkgs.socat}/bin/socat TCP-LISTEN:23343,reuseaddr,fork TCP:100.108.47.83:23343 &
+          ${pkgs.socat}/bin/socat UDP-LISTEN:23343,reuseaddr,fork UDP:100.108.47.83:23343 &
+          ${pkgs.socat}/bin/socat UDP-LISTEN:22232,reuseaddr,fork UDP:100.108.47.83:22232 &
+          wait
+        "
       '';
       Restart = "always";
       User = "nobody";
