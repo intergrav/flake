906b8528b3284e45a3e5640a13450c4297241441
intergrav <devin@devins.page>
2026-01-25 17:35:50 -0500

    fix: bind pds to 0.0.0.0

--- hosts/bluepill/modules/pds.nix
+++ hosts/bluepill/modules/pds.nix
@@ -10,6 +10,7 @@
     environmentFiles = [config.age.secrets.pds.path];
     settings = {
       PDS_PORT = 3000;
+      PDS_HOST = "0.0.0.0";
       PDS_HOSTNAME = "pds.devins.page";
       PDS_SERVICE_HANDLE_DOMAINS = ".pds.devins.page";
       PDS_ADMIN_EMAIL = "devin@devins.page";
